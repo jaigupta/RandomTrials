f3a86a342b2d60bc71dae43d3d732cc53c130156
Jai Gupta <jaigupta0505@gmail.com>
2018-06-16 18:59:25 -0700

    Open and close file events

--- code-editor/main.js
+++ code-editor/main.js
@@ -1,10 +1,36 @@
 const {
   app,
-  BrowserWindow
-} = require('electron');
+  BrowserWindow,
+  dialog,
+  Menu,
+  MenuItem,
+  ipcMain
+} = require('electron')
+
+const path = require('path')
+const fs = require('fs')
+const {
+  openFile
+} = require(path.resolve('js/open_file'))
 
 
 let mainWindow;
+
+class FileManager {
+  openFile(browserWindow, callback) {
+    dialog.showOpenDialog.bind(browserWindow, function(fileNames) {
+      if (!fileNames || fileNames.length == 0) return;
+
+      fs.readFile(fileNames[0], 'utf-8', (err, data) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        callback(fileNames[0], data);
+      });
+    })();
+  }
+}
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -15,17 +41,42 @@ function createWindow() {
   mainWindow.loadFile('index.html');
   mainWindow.webContents.openDevTools();
 
+  var fileManager = new FileManager();
+  const menuTemplate = [{
+    label: 'Electron',
+    submenu: [{
+        label: 'Open File',
+        click: () => {
+          fileManager.openFile(mainWindow, function(fileName, data) {
+            mainWindow.webContents.send('open-file', {
+              fileName: fileName,
+              data: data
+            });
+          });
+        }
+      },
+      {
+        label: 'Save File',
+        click: () => {
+          mainWindow.webContents.send('save-file', {});
+        }
+      }
+    ]
+  }];
+
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
+
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
 }
 
-app.on('ready', createWindow);
+app.on('ready', createWindow)
 
 app.on('window-all-closed', function() {
   app.quit();
 })
-
 
 app.on('activate', function() {
   if (mainWindow === null) createWindow();
